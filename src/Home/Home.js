ed9768e18d788a3fd8979adf4eb67350c5204383
Ezwad Ilham Rahman <30919695+ilhamrahman@users.noreply.github.com>
2020-05-13 17:30:29 -0400

    update

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -2,92 +2,48 @@
 import React, { Component } from 'react';
 import history from './../history';
 import './Home.css';
-
-var validpath = false;
+import x from './X.png';
+import o from './O.png';
 
 export default class MainActivity extends Component {
   
   number=1;
 
-  goUp = (cur_id) => {
+  changeBackground = (cur_id) => {
+    if(this.number%2==1){
+      document.getElementById(cur_id).style.backgroundImage = "url('X.png')";
+    }
+    if(this.number%2==0){
+      document.getElementById(cur_id).style.backgroundImage = o;
+    }
     this.number++;
-      if(cur_id==3){
-        document.getElementById("3").style.backgroundColor ="#00ff00";
-        document.getElementById("4").style.backgroundColor ="#ff0000";
-      }
-      
-      if(cur_id==4){
-        document.getElementById("4").style.backgroundColor ="#00ff00";
-        document.getElementById("5").style.backgroundColor ="#ff0000";
-      }
-      
-      if(cur_id==6){
-        document.getElementById("6").style.backgroundColor ="#00ff00";
-        document.getElementById("7").style.backgroundColor ="#ff0000";
-      }
-      if(cur_id==8){
-        document.getElementById("8").style.backgroundColor ="#00ff00";
-        document.getElementById("9").style.backgroundColor ="#ff0000";
-      }
-      else{
-        console.log("Invalid");
-      }
       
     }
-  
-  goRight = (cur_id) => {
-    this.number++;
-    if(cur_id==1){
-      document.getElementById("1").style.backgroundColor ="#00ff00";
-      document.getElementById("2").style.backgroundColor ="#ff0000";
-    }
-    if(cur_id==2){
-      document.getElementById("2").style.backgroundColor ="#00ff00";
-      document.getElementById("3").style.backgroundColor ="#ff0000";
-    }
-    if(cur_id==5){
-      document.getElementById("5").style.backgroundColor ="#00ff00";
-      document.getElementById("6").style.backgroundColor ="#ff0000";
-    }
-    if(cur_id==7){
-      document.getElementById("7").style.backgroundColor ="#00ff00";
-      document.getElementById("8").style.backgroundColor ="#ff0000";
-    }
-    if(cur_id==9){
-      document.getElementById("9").style.backgroundColor ="#00ff00";
-      document.getElementById("10").style.backgroundColor ="#ff0000";
-      alert("You win!");
-    }
-    else{
-      console.log("Invalid");
-    }
-    
-  }
 
 render() {
   return (
     <body>
     <div className="App">
       <div className="pictures" >
-      <h2 style={{ color: '#00ffff', fontSize: "50px"}}> <em> ILHAM'S MAZE GAME </em> </h2>
+      <h2 style={{ color: '#00ffff', fontSize: "50px"}}> <em> ILHAM'S TIC-TAC-TOE </em> </h2>
     </div>
 
       <header className="pictures">
-      <button><button className="solidblocks" alt="solidblocks" /></button>
-      <button><button className="solidblocks" alt="solidblocks" /></button>
-      <button><button className="solidblocks" alt="solidblocks" /></button>
+      <button><button id="1" className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground("1")}/></button>
+      <button><button id="2" className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground("2")}/></button>
+      <button><button className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground}/></button>
       </header>
 
       <header className="pictures">
-      <button><button className="solidblocks" alt="solidblocks" /></button>
-      <button><button className="solidblocks" alt="solidblocks" /></button>
-      <button><button className="solidblocks" alt="solidblocks" /></button>
+      <button><button className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground}/></button>
+      <button><button className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground}/></button>
+      <button><button className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground}/></button>
       </header>
 
       <header className="pictures">
-      <button><button className="solidblocks" alt="solidblocks" /></button>
-      <button><button className="solidblocks" alt="solidblocks" /></button>
-      <button><button className="solidblocks" alt="solidblocks" /></button>
+      <button><button className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground}/></button>
+      <button><button className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground}/></button>
+      <button><button className="solidblocks" alt="solidblocks" onClick={() => this.changeBackground}/></button>
       </header>
      
        </div>
